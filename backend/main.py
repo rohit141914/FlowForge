60dbad6a972f478ac1fdd92a7974518fd8838081
rohit141914 <rohitnainindian@gmail.com>
2026-03-09 20:56:49 +0530

    fixed cors issue in production

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://flow-forge-beige.vercel.app/"],
+    allow_origins=["http://localhost:3000", "https://flow-forge-beige.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
